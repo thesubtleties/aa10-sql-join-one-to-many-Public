8f6087eb81b9aa4c6ca5c8556492cf0e9f752f99
Steven <stevenglab@gmail.com>
2024-08-10 19:00:10 -0700

    finished

---
+++ sql-practice/step-2.sql
@@ -0,0 +1,11 @@
+-- Step 2
+-- A WHERE clause can filter across any JOINed table, even if it's not in the
+-- final output of what is being SELECTed for.
+-- Your code here 
+SELECT
+    name
+FROM
+    bands
+    JOIN albums ON bands.id = albums.band_id
+WHERE
+    albums.num_sold < 20000;
